7dd24dca9d670d947d1978bbffc40f58dba25edd
smilemax <smilemax@foxmail.com>
2021-04-21 14:37:11 +0800

    将质量流率从kg修正至kg/m

--- particleSampleContainer.C
+++ particleSampleContainer.C
@@ -54,11 +54,11 @@ namespace Foam
 
             if (_position[directionIndex] < samplePosition && position[directionIndex] >= samplePosition)
             {
-                massRateList[hIndex] += p.second.mass();
+                massRateList[hIndex] += p.second.mass() / deltaH;
             }
             else if (_position[directionIndex] > samplePosition && position[directionIndex] <= samplePosition)
             {
-                massRateList[hIndex] -= p.second.mass();
+                massRateList[hIndex] -= p.second.mass() / deltaH;
             }
         }
     }
@@ -121,7 +121,7 @@ namespace Foam
         {
             totalFlowRate += m.second;
         }
-        
+
         return std::to_string(totalFlowRate);
     }
 
